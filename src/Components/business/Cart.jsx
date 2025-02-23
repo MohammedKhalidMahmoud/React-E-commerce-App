6b4472bc29d0e523689f692bf269d412e103c15d
Aboshady48 <aboshady48585@gmail.com>
2025-02-23 12:44:44 +0200

    second

---
+++ src/Components/business/Cart.jsx
@@ -0,0 +1,10 @@
+
+const Cart = () => {
+  return (
+    <div>
+        hello from cart
+    </div>
+  )
+}
+
+export default Cart
---
+++ src/Components/business/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div>
+      aaa
+    </div>
+  )
+}
+
+export default Home
